387c8ffbdb31aec1625aa72fd9b359ecae4cacfa
Roshan Ranabhat <roshanranabhat11@gmail.com>
2023-01-31 08:25:16 +0545

    add update user

--- index.js
+++ index.js
@@ -14,22 +14,32 @@ app.use((req, resp, next) => {
 
 app.post("/user", (req, resp) => {
   const user = req.body;
+  let id = users.length + 1;
+  user.id = id;
   users.push(user);
   resp.status(201).json(user);
 });
 
-app.get('/user', (req, resp) => {
-    resp.json(users);
-})
+app.get("/user", (req, resp) => {
+  resp.json(users);
+});
 
-// {
-//     fullName: 'john',
-//     email: '',
-//     password: '',
-//     address: '',
-//     phone: ''
-
-// }
+app.put("/user/:id", (req, resp) => {
+  let id = req.params.id;
+  let data = req.body;
+  let userIndex = users.findIndex((user, index) => {
+    return user.id == id;
+  });
+  if (userIndex < 0) {
+    return resp.status(404).json({
+      message: "User not found",
+    });
+  }
+  data.id = +id
+  users[userIndex] = data;
+  let updatedData = users[userIndex];
+  resp.json(updatedData);
+});
 
 app.listen(port, () => {
   console.log("Application is running on port: ", port);
