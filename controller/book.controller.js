1db65b590d1c92363552040f6890012eb2b36b67
Roshan Ranabhat <roshanranabhat11@gmail.com>
2023-02-28 08:38:39 +0545

    add get, update delete methods on book controller

--- controller/book.controller.js
+++ controller/book.controller.js
@@ -3,20 +3,36 @@ const BookModel = mongoose.model("Book");
 
 const storeBook = async (req, res) => {
   const data = req.body;
-    let book = new BookModel({
-        ...data
-    });
-    await book.save();
-    res.status(201).json(book);
+  let book = new BookModel({
+    ...data,
+  });
+  await book.save();
+  res.status(201).json(book);
 };
 
-const getAllBook = async (req, res) => {};
+const getAllBook = async (req, res) => {
+  const books = await BookModel.find();
+  res.status(200).json(books);
+};
 
-const getBookById = async (req, res) => {};
+const getBookById = async (req, res) => {
+  const id = req.params.id;
+  const book = await BookModel.findById(id);
+  res.status(200).json(book);
+};
 
-const updateBook = async (req, res) => {};
+const updateBook = async (req, res) => {
+  const id = req.params.id;
+  const data = req.body;
+  const result = await BookModel.findOneAndUpdate({ _id: id }, data);
+  res.status(200).json(result);
+};
 
-const destroyBook = async (req, res) => {};
+const destroyBook = async (req, res) => {
+  const id = req.params.id;
+  await BookModel.findByIdAndDelete(id);
+  res.status(204).json();
+};
 
 module.exports = {
   storeBook,
